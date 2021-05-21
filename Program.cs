0fedbb50dd6404aef846c7c8c1fe41773f75d11b
HPreast <preasths1@gmail.com>
2021-05-21 15:49:18 -0500

    Phase 1

--- Program.cs
+++ Program.cs
@@ -1,12 +1,3 @@
 ﻿using System;
 
-namespace MagicMoose
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+Console.Write("Ask me a question.");
